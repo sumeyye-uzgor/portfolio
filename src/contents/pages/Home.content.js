111a737a79997e967ef287afbe1c70e02f96c4b0
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-05-31 17:14:15 +0300

    homepage is styled except functionality of buttons

--- src/contents/pages/Home.content.js
+++ src/contents/pages/Home.content.js
@@ -2,14 +2,19 @@
 const HomeContent = [
     {
         lang: "TR",
-        title: "Ben Sümeyye Üzgör.",
+        title1: "Ben",
+        title2: "Sümeyye",
+        title3: "Üzgör.",
+        title4: "",
         hire: "Bizimle çalışmak ister misin?",
         website: "Bir websitesi yaptırmak istiyorum.",
     },
-
     {
         lang: "EN",
-        title: "I am Sümeyye Üzgör.",
+        title1: "I",
+        title2: "am",
+        title3: "Sümeyye",
+        title4: "Üzgör.",
         hire: "I want to hire you.",
         website: "I need a website.",
     }
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,10 +1,41 @@
 import React from "react"
-const Home = () => {
+import { Container, Row, Col, Card, Button } from "react-bootstrap"
+import { connect } from "react-redux";
+import HomeContent from "../../contents/pages/Home.content.js"
+import myImg from "../../assets/foto.jpg"
+
+const Home = ({ language }) => {
+    const content = HomeContent.filter(item => item.lang === language)[0]
     return (
-        <div>
-            Home
-        </div>
+        <Container>
+            <Row className="align-items-center justify-content-around">
+                <Col xs={11} md={5} className="m-10">
+                    {/* <Card style={{ margin: "10px" }} >
+                        <Card.Body> */}
+                    <Row className="justify-content-end" style={{ fontSize: "45px" }}>{content.title1}</Row>
+                    <Row className="justify-content-end" style={{ fontSize: "45px" }}>{content.title2}</Row>
+                    <Row className="justify-content-end" style={{ fontSize: "45px" }}>{content.title3}</Row>
+                    <Row className="justify-content-end" style={{ fontSize: "45px" }}>{content.title4}</Row>
+
+
+                    <Row className="justify-content-end" style={{ fontSize: "15px" }}>
+                        <Button variant="light" className="mr-2">{content.hire}</Button>
+                        <Button variant="light">{content.website}</Button>
+                    </Row>
+                    {/* </Card.Body>
+                    </Card> */}
+                </Col>
+                <Col xs={11} md={6}>
+                    <Card style={{ margin: "10px", width: "300px" }}>
+                        <img variant="top" src={myImg} alt="sumeyye uzgor" />
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
-
-export default Home;
+const mapStateToProps = (state) => {
+    const language = state.lang
+    return { language }
+}
+export default connect(mapStateToProps)(Home);
