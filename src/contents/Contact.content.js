2d1eae9d4b0a9f5685a933d826ccc860d465f8e7
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-05-31 22:26:20 +0300

    layout of contact page is created

---
+++ src/contents/Contact.content.js
@@ -0,0 +1,22 @@
+
+const ContactContent = [
+    {
+        lang: "TR",
+        name: "İsim",
+        email: "E-posta",
+        subject: "Konu",
+        message: "Mesaj",
+        button: "Gönder!",
+        outMessage: "En kısa sürede size dönüş yapacağım!",
+    },
+    {
+        lang: "EN",
+        name: "Name",
+        email: "E-mail",
+        subject: "Subject",
+        message: "Message",
+        button: "Submit!",
+        outMessage: "I will respond you as soon as possible!",
+    }
+]
+export default ContactContent
--- src/pages/Contact.page.jsx
+++ src/pages/Contact.page.jsx
@@ -1,12 +1,52 @@
 import React from "react"
+import { Container, Row, Col, Form, Button } from "react-bootstrap"
+import { connect } from "react-redux";
+import ContactContent from "../contents/Contact.content"
+// import myImg from "../assets/me2.jpeg"
 
-
-function Contact() {
+const Contact = ({ language }) => {
+    const content = ContactContent.filter(item => item.lang === language)[0]
     return (
-        <div className="Home">
-            Contact
-        </div>
+        <Container>
+            <Row className="align-items-center justify-content-center">
+                <Col xs={11} md={8}>
+                    <Form>
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label size="sm">{content.email}</Form.Label>
+                                    <Form.Control size="sm" type="email" placeholder={content.email} />
+                                </Form.Group>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Form.Group className="mb-3" controlId="formBasicText">
+                                    <Form.Label size="sm">{content.name}</Form.Label>
+                                    <Form.Control size="sm" type="text" placeholder={content.name} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label size="sm">{content.subject}</Form.Label>
+                            <Form.Control size="sm" type="text" placeholder={content.subject} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label size="sm">{content.message}</Form.Label>
+                            <Form.Control size="sm" as="textarea" placeholder={content.message} style={{ height: '100px' }} />
+                        </Form.Group>
+                        <Button variant="light" type="submit">
+                            {content.button}
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
-
-export default Contact;
+const mapStateToProps = (state) => {
+    const language = state.lang
+    return { language }
+}
+export default connect(mapStateToProps)(Contact);
