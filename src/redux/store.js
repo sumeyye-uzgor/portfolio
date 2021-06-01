46d34e56b55f8d39173a7a4d278c3f0f917ccf7a
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-06-01 14:54:25 +0300

    package.json is updated for needed packages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import rootReducer from './reducer';
 
 import thunk from 'redux-thunk';
 
-const middlewares = [thunk, logger];
-
-// if (process.env.NODE_ENV === 'development') {
-//     middlewares.push(logger);
-// }
+const middlewares = [thunk];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export default store
