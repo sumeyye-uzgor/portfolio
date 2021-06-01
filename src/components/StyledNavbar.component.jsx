ee7a40111e52821ee2d951d9c47962a4b23e607a
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-06-01 14:34:07 +0300

    language setting is done with navbar

--- src/components/StyledNavbar.component.jsx
+++ src/components/StyledNavbar.component.jsx
@@ -1,12 +1,19 @@
 import React from "react"
-import { Navbar, Container, Nav } from "react-bootstrap"
+import { Navbar, Container, Nav, Button } from "react-bootstrap"
 import { connect } from "react-redux"
 import NavbarContent from "../contents/Navbar.content"
 import "./StyledNavbar.styles.css"
 
-function StyledNavbar({ language }) {
-    const content = NavbarContent.filter(item => item.lang === language)[0]
+import { setLanguage } from "../redux/actions"
+import { ReactComponent as Turkish } from "../assets/tr.svg"
+import { ReactComponent as English } from "../assets/usa.svg"
 
+
+function StyledNavbar({ language, setLanguage }) {
+    const content = NavbarContent.filter(item => item.lang === language)[0]
+    const handleChange = (inputLang) => {
+        setLanguage(inputLang)
+    }
     return (
         <Navbar sticky="top" expand="sm" className="styledNav">
             <Container>
@@ -14,12 +21,14 @@ function StyledNavbar({ language }) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        {/* <Nav.Link href="/">{content.home}</Nav.Link> */}
-                        {/* <Nav.Link href="/">SU</Nav.Link> */}
                         <Nav.Link href="/projects">{content.projects}</Nav.Link>
                         <Nav.Link href="/comments">{content.comments}</Nav.Link>
                         <Nav.Link href="/aboutme">{content.about}</Nav.Link>
                         <Nav.Link href="/contact">{content.contact}</Nav.Link>
+                        <Nav.Link href="/contact">{content.contact}</Nav.Link>
+                        <Button className="p-0 customButton" onClick={() => handleChange("TR")}><Turkish width="30px" height="20px" /></Button>
+                        <Button className="p-0 customButton" onClick={() => handleChange("EN")}><English width="30px" height="20px" /></Button>
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -31,4 +40,10 @@ const mapStateToProps = (state) => {
     const language = state.lang
     return { language }
 }
-export default connect(mapStateToProps)(StyledNavbar);
+const mapDispatchToProps = dispatch => {
+    return {
+        setLanguage: (lang) => dispatch(setLanguage(lang))
+
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(StyledNavbar);
